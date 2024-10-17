d774996234ca7205b62e6eeb180bb461161daab9
OrnelaNochetti <ornelanochetti@gmail.com>
2024-10-17 20:57:43 -0300

    agrego los archivos del tp

---
+++ tp/mesas.js
@@ -0,0 +1,34 @@
+const mesas = [
+    {mesa: 1,
+        disponibilidad: false
+    },
+    {mesa: 2,
+        disponibilidad: false
+    },
+    {mesa: 3,
+        disponibilidad: false
+    },
+    {mesa: 4,
+        disponibilidad: false
+    },
+    {mesa: 5,
+        disponibilidad: false
+    },
+    {mesa: 6,
+        disponibilidad: false
+    },
+    {mesa: 7,
+        disponibilidad: false
+    },
+    {mesa: 8,
+        disponibilidad: false
+    },
+    {mesa: 9,
+        disponibilidad: false
+    },
+    {mesa: 10,
+        disponibilidad: false
+    },
+]
+
+export {mesas}
---
+++ tp/personas.js
@@ -0,0 +1,34 @@
+const personas_por_mesa = [
+    {mesa: 1,
+        personas: 2
+    },
+    {mesa: 2,
+        personas: 2
+    },
+    {mesa: 3,
+        personas: 5
+    },
+    {mesa: 4,
+        personas: 3
+    },
+    {mesa: 5,
+        personas: 2
+    },
+    {mesa: 6,
+        personas: 4
+    },
+    {mesa: 7,
+        personas: 2
+    },
+    {mesa: 8,
+        personas: 3
+    },
+    {mesa: 9,
+        personas: 3
+    },
+    {mesa: 10,
+        personas: 4
+    },
+]
+
+export {personas_por_mesa}
